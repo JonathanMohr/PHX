c489f25527d42c9f666e10cfd95fc6642957a412
Jonathan <mohrjonathan09@gmail.com>
2026-07-03 18:08:18 +0200

    .

--- tools/phx/disk.h
+++ tools/phx/disk.h
@@ -20,7 +20,17 @@ extern "C" {
 
 typedef struct Disk
 {
-    uint64_t sectorSize;
+    /** Returns how many sectors have been read */
+    uint64_t (*readSectors)(struct Disk* disk, uint8_t* buffer, uint64_t lba, uint64_t count);
+    /** Returns how many sectors have been written */
+    uint64_t (*writeSectors)(struct Disk* disk, const uint8_t* buffer, uint64_t lba, uint64_t count);
+
+    uint64_t (*getSectorCount)(struct Disk* disk);
+    uint64_t (*getSectorSize)(struct Disk* disk);
+
+    void (*close)(struct Disk* disk);
+
+    void* data;
 } Disk;
 
 #ifdef __cplusplus
